3bedf60863a38be19f1eaa41709bec234ae69d52
Matt Schick <matt@schickm.com>
2015-02-10 15:37:06 -0600

    initial commit of project

---
+++ client/stealthecontent.js
@@ -0,0 +1,60 @@
+Template.main.helpers({
+    data: {
+        search: "boring",
+        dictionary: {
+            syllables: ["bor", "ing"],
+            phonetic: "/ˈbôriNG/",
+            partOfSpeech: "adjective",
+            definition: "not interesting; tedious.",
+            context: "I've got a boring job in an office",
+            synonyms: ["tedious", "dull", "monotonous", "repetitive"],
+        },
+        resultCount: 199000000,
+        searchTime: 0.29,
+        results: [
+            {
+                type: "website",
+                title: "Boring Synonyms, Boring Antonyms | Thesaurus.com",
+                url: "http://www.thesaurus.com/browse/boring",
+                excerpt: "Synonyms for boring at Thesaurus.com with free online thesaurus, antonyms, and definitions. Dictionary and Word of the Day." 
+            },
+            {
+                type: "website",
+                title: "Boring | Define Boring at Dictionary.com",
+                url: "http://dictionary.reference.com/browse/boring",
+                excerpt: "Dennis's life is boring until Vogue magazine introduces him to fashion. It gets a little boring being in a hospital all day. It sounds exciting, but the funny thing about paleontology is that it's very boring. And as they walked or rode, and looked at them, they could talk without too much boring each other."
+            },
+            {
+                type: "location",
+                title: "The Wicker Park Secret Agent Supply Co.",
+                url: "http://www.826chi.org/",
+                rating: 4.4,
+                reviews: 9,
+                address: "1276 North Milwaukee Avenue, Chicago, IL 60622",
+                phone: "(773) 772-8108"
+            },
+            {
+                type: "images",
+                imageUrls: [
+                    "http://positiveresultsmarketing.com/wp-content/uploads/2014/01/Lost-interest.jpg",
+                    "http://blog.hittail.com/wp-content/uploads/2014/05/3410197-boring.jpg",
+                    "http://i.huffpost.com/gen/352503/thumbs/r-SCHOOL-BORING-large570.jpg",
+                    "http://yourschoolmarketing.com/wp-content/uploads/2011/12/bored-baby.jpg",
+                    "http://cdn2.hubspot.net/hub/53/file-23115846-jpg/blog/images/boring-marketing-content.jpg?t=1423367392232"
+                ]
+            }
+        ],
+        relatedSearches: [
+            "meaning",
+            "things",
+            "synonym",
+            "crossword clue",
+            "games",
+            "antonym",
+            "quotes",
+            "bees"
+        ],
+        pages: 10,
+        currentPage: 1
+    }
+});
