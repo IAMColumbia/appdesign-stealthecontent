07f974c3b8784b187bb3c1f903770170860b8bea
Tony V. Le <tvledesign@gmail.com>
2015-02-11 17:10:34 -0600

    Filled in stealthecontent.js with Twitter content

--- client/stealthecontent.js
+++ client/stealthecontent.js
@@ -1,60 +1,61 @@
 Template.main.helpers({
     data: {
-        search: "boring",
-        dictionary: {
-            syllables: ["bor", "ing"],
-            phonetic: "/ˈbôriNG/",
-            partOfSpeech: "adjective",
-            definition: "not interesting; tedious.",
-            context: "I've got a boring job in an office",
-            synonyms: ["tedious", "dull", "monotonous", "repetitive"],
-        },
-        resultCount: 199000000,
-        searchTime: 0.29,
-        results: [
+        status: "What's hapening?",
+        tweets: [
             {
-                type: "website",
-                title: "Boring Synonyms, Boring Antonyms | Thesaurus.com",
-                url: "http://www.thesaurus.com/browse/boring",
-                excerpt: "Synonyms for boring at Thesaurus.com with free online thesaurus, antonyms, and definitions. Dictionary and Word of the Day." 
+                retweeted: "Figment retweeted",
+                profilePicture: "https://pbs.twimg.com/profile_images/491043866145869825/nyWLShZW_bigger.jpeg",
+                name: "Emily Gover",
+                username: "@Emily_EasyBib",
+                time: "4h",
+                status: ".@Biblionasium is great for Ss age 6-13; for older kids, try @EpicReads, @figment, and @VocabularyCom. #EasyBibEvents #teacheredchat #edtech",
+                retweets: 3,
+                favorites: 5
             },
             {
-                type: "website",
-                title: "Boring | Define Boring at Dictionary.com",
-                url: "http://dictionary.reference.com/browse/boring",
-                excerpt: "Dennis's life is boring until Vogue magazine introduces him to fashion. It gets a little boring being in a hospital all day. It sounds exciting, but the funny thing about paleontology is that it's very boring. And as they walked or rode, and looked at them, they could talk without too much boring each other."
+                profilePicture: "https://pbs.twimg.com/profile_images/1213972918/IGM_Square_Logo_Only_bigger.png",
+                name: "Indie Game Mag",
+                username: "@indiegamemag",
+                time: "37m",
+                status: "Nobody likes Driver's Ed courses. But if you could practice in Space, they'd be exponentially awesomer: https://www.kickstarter.com/projects/scottmeyer/blue-collar-astronaut-physics-based-hand-drawn-sat?ref=card … #indiegdev",
+                retweets: 1,
+                viewMedia: "https://twitter.com/indiegamemag/status/565631498771267584"
             },
             {
-                type: "location",
-                title: "The Wicker Park Secret Agent Supply Co.",
-                url: "http://www.826chi.org/",
-                rating: 4.4,
-                reviews: 9,
-                address: "1276 North Milwaukee Avenue, Chicago, IL 60622",
-                phone: "(773) 772-8108"
+                profilePicture: "https://pbs.twimg.com/profile_images/3767727275/787167430f7f20ba0a611279eb6b7f29_bigger.jpeg",
+                name: "Three If By Space",
+                username: "@ThreeIfBySace",
+                time: "40m",
+                status: "Three If By Space - Daily News is out! http://paper.li/ThreeIfBySpace/1362606951 … Stories via @TaylorCubed09 @jennilooch @AnesKanya"
             },
             {
-                type: "images",
-                imageUrls: [
-                    "http://positiveresultsmarketing.com/wp-content/uploads/2014/01/Lost-interest.jpg",
-                    "http://blog.hittail.com/wp-content/uploads/2014/05/3410197-boring.jpg",
-                    "http://i.huffpost.com/gen/352503/thumbs/r-SCHOOL-BORING-large570.jpg",
-                    "http://yourschoolmarketing.com/wp-content/uploads/2011/12/bored-baby.jpg",
-                    "http://cdn2.hubspot.net/hub/53/file-23115846-jpg/blog/images/boring-marketing-content.jpg?t=1423367392232"
-                ]
+                retweeted: "Official_GDC retweeted",
+                profilePicture: "https://pbs.twimg.com/profile_images/543564604190646272/9OIp8HvK_bigger.jpeg",
+                name: "Tim Schafer",
+                username: "@TimOfLegend",
+                time: "48m",
+                status: "Vote now for the 2015 GDC Audience Award! Power to the people! Until the 13th, when your power is gone. :(  http://www.gdconf.com/news/vote_now_for_the_2015_game_dev.html …",
+                retweets: 16,
+                favorites: 13
+            },
+            {
+                profilePicture: "https://pbs.twimg.com/profile_images/440430797732855808/FRclruP0_bigger.png",
+                name: "Erin Robinson",
+                username: "@Livelyivy",
+                time: "53m",
+                status: "Mail merge is hard (especially if you're a 'technological solutions' company that doesn't proofread)",
+                favorites: 7,
+                viewPhoto: "https://pbs.twimg.com/media/B9mETzIIQAUiGlK.png"
+            },
+            {
+                retweeted: "NSCS retweeted",
+                profilePicture: "https://pbs.twimg.com/profile_images/442474791639846912/09X-sDg9_bigger.jpeg",
+                name: "ScholarCon",
+                username: "@ScholarCon",
+                time: "Feb 4",
+                status: "Early bird registration for the best gathering of the brightest collegiate minds? Yes, please! http://www.scholarcon.org",
+                retweets: 2
             }
-        ],
-        relatedSearches: [
-            "meaning",
-            "things",
-            "synonym",
-            "crossword clue",
-            "games",
-            "antonym",
-            "quotes",
-            "bees"
-        ],
-        pages: 10,
-        currentPage: 1
+        ]
     }
 });
