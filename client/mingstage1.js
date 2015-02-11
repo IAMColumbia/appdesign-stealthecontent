2b84756f8718769d8e07ae04df2d970d19c462b4
bill668 <yumingyuan668@gmail.com>
2015-02-11 17:28:31 -0600

    Ming stage1
    ming

---
+++ client/mingstage1.js
@@ -0,0 +1,151 @@
+Template.main.helpers({
+    data: {
+        search: "girlfriend",
+        resultCount: 260,960,
+        currentPageResult: [1,16],
+        results: 
+        [
+            {
+                title: "Virtual Girlfriend - Verity",
+                type: "Apps & Games",
+                productFeatures:"Vitual Girlfriend",
+                rank:5.0,
+                peopleWhoVote:1,
+                url: "http://www.amazon.com/Bivvybuddy-Virtual-Girlfriend-Verity/dp/B00ONB0GH4/ref=sr_1_1?ie=UTF8&qid=1423693584&sr=8-1&keywords=girlfriend",
+                imgUrl:"http://ecx.images-amazon.com/images/I/71F-lJ2ZniL._AA160_.png",
+                seller:"Bivvybuddy",
+                price: 0.0,
+                originPrice:0.0,
+                prime:false,
+                excerpt: "Available instantly on your connected Android device",
+                startSellingDate:["Oct","18","2014"]
+            },
+
+            {
+                title: "Phoney Girlfriend",
+                type: "Apps & Games",
+                productFeatures:"Vitual Girlfriend",
+                rank:4.5,
+                peopleWhoVote:3,
+                url: "http://www.amazon.com/Baller-Industries-Phoney-Girlfriend/dp/B00QYCGVQK/ref=sr_1_2?ie=UTF8&qid=1423693584&sr=8-2&keywords=girlfriend",
+                imgUrl:"http://ecx.images-amazon.com/images/I/81tqdSMktzL._AA160_.png",
+                seller:"Baller Industries",
+                price: 0.0,
+                originPrice:0.0,
+                prime:false,
+                excerpt: "Available instantly on your connected Android device",
+                startSellingDate:["Dec","13","2014"]
+            },
+
+            {
+                title: "I Love My Crazy Girlfriend Unisex T-shirt Couple Shirts",
+                type: "Clothing, Shoes & Jewelry",
+                productFeatures:"",
+                rank:4.4,
+                peopleWhoVote:22,
+                url: "http://www.amazon.com/Girlfriend-Unisex-T-Shirt-Couple-X-Large/dp/B00MMU87VU/ref=sr_1_3?ie=UTF8&qid=1423693584&sr=8-3&keywords=girlfriend",
+                imgUrl:"http://ecx.images-amazon.com/images/I/41eCay1GsBL._AA160_.jpg",
+                seller:"Shop4Ever",
+                price: [5.29,13.23],
+                originPrice:17.99,
+                prime:true,
+                excerpt: "Some sizes/colors are Prime eligible", 
+                startSellingDate:""
+            },
+
+            {
+                title: "Rate My Girlfriend",
+                type: "Apps & Games",
+                productFeatures:"Rate other peoples girlfriends",
+                rank:3.7,
+                peopleWhoVote:3,
+                url: "http://www.amazon.com/Stinky-Dog-Studios-Rate-Girlfriend/dp/B007QEHH7M/ref=sr_1_4?ie=UTF8&qid=1423693584&sr=8-4&keywords=girlfriend",
+                imgUrl:"http://ecx.images-amazon.com/images/I/41bU-wIJQVL._AA160_.png",
+                seller:"Stinky Dog Studios",
+                price: 0.0,
+                originPrice:0.0,
+                prime:false,
+                excerpt: "Available instantly on your connected Android device", 
+                startSellingDate:["Mar","31","2012"]
+            },
+                       
+            {
+                type: "video shorts",
+                videoUrls: [
+                    
+                {
+                    title: "Time Always Moving - The Girlfriend Trailer",
+                    imgUrl:"http://ecx.images-amazon.com/images/I/81geDYXmgZL._SCLZZZZZZZ__SR160,90_PKvap-overlay-play-small_OU01_.png",
+                    url:"www.amazon.com/gp/product/B00J5S31KO/ref=sr_acs_va_item_0?ie=UTF8&qid=1423693584&sr=8-8-acs"
+                },
+                {
+                    title: "Girlfriend - Trailer",
+                    imgUrl:"http://ecx.images-amazon.com/images/I/81mjxP6UUaL._SCLZZZZZZZ__SR160,90_PKvap-overlay-play-small_OU01_.png",
+                    url:"/gp/product/B00H4I06QO/ref=sr_acs_va_item_1?ie=UTF8&qid=1423693584&sr=8-8-acs"
+                },
+                {
+                    title:"Someone Still Loves You Boris Yeltsin - Nightwater...",
+                    imgUrl:"http://ecx.images-amazon.com/images/I/61tLK6r0miL._SCLZZZZZZZ__SR160,90_PKvap-overlay-play-small_OU01_.jpg",
+                    url:"www.amazon.com/gp/product/B00JU957YQ/ref=sr_acs_va_item_2?ie=UTF8&qid=1423693584&sr=8-8-acs"
+                },
+                {
+                    title:"Disconnect: Girlfriend",
+                    imgUrl:"http://ecx.images-amazon.com/images/I/61M7g+x6BkL._SCLZZZZZZZ__SR160,90_PKvap-overlay-play-small_OU01_.jpg",
+                    url:"www.amazon.com/gp/product/B00K2GV1C8/ref=sr_acs_va_item_3?ie=UTF8&qid=1423693584&sr=8-8-acs"
+                }
+
+
+                ]
+            }
+        ],
+        relatedSearches: 
+        [
+            "boyfriend",
+            "girlfriend gift",
+            "irlfriend gifts",
+        ],
+        pages: 20,
+        currentPage: 1,
+
+        recentlyViewed:
+        {
+
+            results:
+            [
+                {
+                    title:"Dueling Dragon and Tiger Headband",
+                    imgUrl:"http://ecx.images-amazon.com/images/I/31j2tXKqBDL._SL500_SL135_.jpg",
+                    rank:4.0,
+                    peopleWhoVote:6,
+                    price:6.95,
+                    prime:false
+                },
+            ],
+
+            pages:6,
+            currentPage:1,
+            historyView:
+            [
+                {
+                    imgUrl:"http://ecx.images-amazon.com/images/I/31QHRCPMZ7L._SL500_SS50_.jpg",
+                },
+
+                {
+                    imgUrl:"http://ecx.images-amazon.com/images/I/31FODO8sU-L._SL500_SS50_.jpg",
+                },
+
+                {
+                    imgUrl:"http://ecx.images-amazon.com/images/I/415DIQ8iiKL._SL500_SS50_.jpg",
+                },
+
+                {
+                    imgUrl:"http://ecx.images-amazon.com/images/I/41K1VCH3FFL._SL500_SS50_.jpg",
+                },
+
+            ],
+
+        },
+
+
+    }
+});
