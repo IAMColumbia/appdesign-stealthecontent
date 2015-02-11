e49e0497055aef0c2e977a533bd06d0e5c298b4a
Maria Faulisi <maria.faulisi@loop.colum.edu>
2015-02-11 17:10:17 -0600

    buzzfeed data

--- client/stealthecontent.js
+++ client/stealthecontent.js
@@ -1,60 +1,80 @@
 Template.main.helpers({
     data: {
-        search: "boring",
-        dictionary: {
-            syllables: ["bor", "ing"],
-            phonetic: "/ˈbôriNG/",
-            partOfSpeech: "adjective",
-            definition: "not interesting; tedious.",
-            context: "I've got a boring job in an office",
-            synonyms: ["tedious", "dull", "monotonous", "repetitive"],
-        },
-        resultCount: 199000000,
-        searchTime: 0.29,
-        results: [
+        search: "entertainment",
+        featuredStories: [
             {
-                type: "website",
-                title: "Boring Synonyms, Boring Antonyms | Thesaurus.com",
-                url: "http://www.thesaurus.com/browse/boring",
-                excerpt: "Synonyms for boring at Thesaurus.com with free online thesaurus, antonyms, and definitions. Dictionary and Word of the Day." 
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/11/13/campaign_images/webdr07/laverne-cox-will-play-a-transgender-power-lawyer--2-12290-1423678369-9_big.jpg",
+                title: "Laverne Cox Was Just Cast In a New CBS Pilot",
+                excerpt: "The Orange is the New Black fan favorite will co-star in Doubt, a CBS drama in contention for the 2015 fall TV schedule.",
+                author: "Jarett Wieselman",
+                responses: 51
             },
             {
-                type: "website",
-                title: "Boring | Define Boring at Dictionary.com",
-                url: "http://dictionary.reference.com/browse/boring",
-                excerpt: "Dennis's life is boring until Vogue magazine introduces him to fashion. It gets a little boring being in a hospital all day. It sounds exciting, but the funny thing about paleontology is that it's very boring. And as they walked or rode, and looked at them, they could talk without too much boring each other."
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/11/13/campaign_images/webdr04/the-trainwreck-trailer-is-here-and-itll-make-your-2-24173-1423680551-9_big.jpg",
+                title: "The 'Trainwreck' Trailer Is Here And It'll Make Your Wednesday",
+                excerpt: "The new comedy, due out this summer, is filled with tons of laughs and tons of cameos.",
+                author: "Emily Orley",
+                responses: 42
             },
             {
-                type: "location",
-                title: "The Wicker Park Secret Agent Supply Co.",
-                url: "http://www.826chi.org/",
-                rating: 4.4,
-                reviews: 9,
-                address: "1276 North Milwaukee Avenue, Chicago, IL 60622",
-                phone: "(773) 772-8108"
-            },
-            {
-                type: "images",
-                imageUrls: [
-                    "http://positiveresultsmarketing.com/wp-content/uploads/2014/01/Lost-interest.jpg",
-                    "http://blog.hittail.com/wp-content/uploads/2014/05/3410197-boring.jpg",
-                    "http://i.huffpost.com/gen/352503/thumbs/r-SCHOOL-BORING-large570.jpg",
-                    "http://yourschoolmarketing.com/wp-content/uploads/2011/12/bored-baby.jpg",
-                    "http://cdn2.hubspot.net/hub/53/file-23115846-jpg/blog/images/boring-marketing-content.jpg?t=1423367392232"
-                ]
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/11/14/campaign_images/webdr02/the-man-whos-made-masterpieces-out-of-stick-figur-2-31867-1423683780-33_big.jpg",
+                title: "The Man Who's Made Masterpieces Out Of Stick Figures",
+                excerpt: "With his latest movie, World of Tomorrow, Don Hertzfeldt made Sundance Film Festival history. The animator talks to BuzzFeed News at the start of what’s looking to be his biggest year yet.",
+                author: "Alison Willmore",
+                responses: 14
             }
         ],
-        relatedSearches: [
-            "meaning",
-            "things",
-            "synonym",
-            "crossword clue",
-            "games",
-            "antonym",
-            "quotes",
-            "bees"
-        ],
-        pages: 10,
-        currentPage: 1
+        latestInEntertainment: [
+            {
+                imageUrl: "http://s3-ak.buzzfed.com/static/2015-02/11/12/campaign_images/webdr01/obama-defends-his-legacy-these-are-the-kinds-of-t-2-17832-1423675175-21.jpg",
+                title: "Obama Defends His Legacy: 'These Are The Kinds Of Things You Learn'",
+                excerpt: "In the BuzzFeed News interview with President Barack Obama, the president celebrates Obamacare and marriage equality, blasts Vladimir Putin and Staples, and sets down a marker for Hillary Clinton.",
+                author: {
+                    imageUrl: "http://s3-ak.buzzfed.com/static/user_images/web04/2012/4/5/2/bensmith-11556-1333606690-44_large.jpg",
+                    staffTitle: "BuzzFeed Editor-in-Chief",
+                    name: "Ben Smith"
+                }
+            },
+            {
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2014-12/4/18/campaign_images/webdr08/eugene-levy-and-catherine-ohara-look-back-on-thei-2-10521-1417736712-10.jpg",
+                title: "Eugene Levy And Catherine O'Hara Look Back On Their 40-Year Friendship",
+                excerpt: "After meeting as up-and-coming comedians in the early '70s, Eugene Levy and Catherine O’Hara brought their signature brand of humor to Hollywood and — over the next 40 years — became a beloved on-screen duo. They look back on four decades of friendship, collaboration, and kissing.",
+                author: "Jarett Wieselman",
+                postTime: "1 hour ago",
+                responses: 30
+            },
+            {
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/11/11/campaign_images/webdr08/yes-all-knope-2-7098-1423672039-13.jpg",
+                title: "12 Awesomely Feminist Moments From This Week's Episodes Of 'Parks And Rec'",
+                excerpt: "Leslie Knope's (and Amy Poehler's) perfume is officially made of male tears. WARNING: THIS POST IS ENTIRELY FILLED WITH SPOILERS.",
+                author: "Sami Main",
+                postTime: "2 hours ago",
+                responses: 70
+            },
+            {
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/10/21/campaign_images/webdr04/brian-williams-suspended-for-six-months-without-p-2-20127-1423620903-18.jpg",
+                title: "Brian Williams Suspended For Six Months Without Pay Over Iraq Story",
+                excerpt: "The NBC Nightly News anchor came under fire last week after U.S. military personnel questioned his story that he was aboard a downed helicopter.",
+                author: "Jim Dalrymple II",
+                postTime: "13 hours ago",
+                responses: 17
+            },   
+            {
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/11/17/campaign_images/webdr04/why-the-new-spider-man-must-be-peter-parker-and-o-2-12642-1423693085-7.jpg",
+                title: "Why The New Spider-Man Must Be Peter Parker And Only Peter Parker",
+                excerpt: "Miles Morales has been an exciting, much-needed addition to the Marvel Comics universe, but it's Peter Parker who needs to be in the films.",
+                author: "Ira Madison III",
+                postTime: "17 hours ago",
+                responses: 87
+            }, 
+            {
+                imageUrl: "http://s3-static-ak.buzzfed.com/static/2015-02/10/22/campaign_images/webdr08/csi-fans-were-mistakenly-led-to-believe-the-show--2-8211-1423624633-4.jpg",
+                title: "'CSI' Fans Were Mistakenly Led To Believe The Show Was Canceled",
+                excerpt: "CBS accidentally promoted the CSI 'series finale' during an episode of NCIS.",
+                author: "Jarett Wieselman",
+                postTime: "19 hours ago",
+                responses: 25
+            }
+        ]
     }
 });
