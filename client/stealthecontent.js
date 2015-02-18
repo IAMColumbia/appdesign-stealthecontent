3bc1f4a66612b65498819347ce81947a4bab6b69
Tony V. Le <tvledesign@gmail.com>
2015-02-17 23:08:19 -0600

    Added in css and fixed js

--- client/stealthecontent.js
+++ client/stealthecontent.js
@@ -1,44 +1,58 @@
 Template.main.helpers({
     data: {
-        video: "url...", // couldn'T be found easily using inspect element
-        headline: {
-            description: "'American Sniper' murder trial begins",
-            link: "http://www.cnn.com/videos/us/2015/02/11/newday-dnt-lavandera-eddie-ray-routh-trial-chris-kyle.cnn"
+            video: "https://www.youtube.com/embed/phbIg8gpvkM",
+            headlineDescription: "'American Sniper' murder trial begins",
+            headlineLink: "http://www.cnn.com/videos/us/2015/02/11/newday-dnt-lavandera-eddie-ray-routh-trial-chris-kyle.cnn",
+            slider: [
+                {
+                    headline: "American Sniper' murder trial begins",
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150211082706-newday-dnt-lavandera-eddie-ray-routh-trial-chris-kyle-00013612-large-169.jpg"
+                },
+                {
+                    headline: "Muslim students killed in Chapel Hill shooting",
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150211051557-chapel-hill-triple-murder-large-169.jpg"
+                },
+                {
+                    headline: "Obama predicts same-sex marriage victory",
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150211074813-obama-buzzfeed-same-sex-marriage-large-169.png"
+                },
+                {
+                    headline: "The world grieves with us",
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150206171146-03-kayla-muller-0206-large-169.jpg"
+                },
+                {
+                    headline: "U.S., UK pull embassy staffers out of Yeme",
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150206131757-01-yemen-unrest-0206-large-169.jpg"
+                },
+                {
+                    headline: "ISIS giving terror recruits new instructions",
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150210173802-isis-manual-lead-pkg-02-10-large-169.jpg"
+                }
+            ],
+            popularStories: [
+                {
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150211004149-orig-jon-stewart-leaving-daily-show-00020622-medium-169.jpg",
+                    description: "See Stewart announce departure from 'The Daily Show",
+                    link: "http://www.cnn.com/videos/entertainment/2015/02/11/orig-jon-stewart-leaving-daily-show.comedy-central/video/playlists/most-popular-domestic/"
+                },
+                {
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150206133834-01-kayla-mueller-0206-medium-169.jpg",
+                    description: "Childhood friend: Kayla Mueller 'was joy",
+                    link: "http://www.cnn.com/videos/world/2015/02/10/lead-bts-kayla-mueller-family-and-friends-press-conference.cnn/video/playlists/most-popular-domestic/"
+                },
+                {
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150210072940-newday-australia-car-chase-medium-169.png",
+                    description: "High-speed car chase has dramatic ending",
+                    link: "http://www.cnn.com/videos/us/2015/02/10/newday-australia-car-chase.cnn/video/playlists/most-popular-domestic/"
+                },
+                {
+                    image: "http://i2.cdn.turner.com/cnnnext/dam/assets/150210210718-ac-dnt-kaye-nick-gordon-profile-00001513-medium-169.jpg",
+                    description: "Who is Nick Gordon?",
+                    link: "http://www.cnn.com/videos/entertainment/2015/02/10/ac-dnt-kaye-nick-gordon-profile.cnn/video/playlists/most-popular-domestic/"
+                }
+
+            ],
+            weatherLocation: "New York",
+            weatherTemperature: 32
         }
-        slider: {
-            headline: ["'American Sniper' murder trial begins", 
-                       "Muslim students killed in Chapel Hill shooting", 
-                       "Obama predicts same-sex marriage victory",
-                        "'The world grieves with us'", 
-                        "U.S., UK pull embassy staffers out of Yemen", 
-                        "ISIS giving terror recruits new instructions" ],
-            image: ["http://i2.cdn.turner.com/cnnnext/dam/assets/150211082706-newday-dnt-lavandera-eddie-ray-routh-trial-chris-kyle-00013612-large-169.jpg",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150211051557-chapel-hill-triple-murder-large-169.jpg",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150211074813-obama-buzzfeed-same-sex-marriage-large-169.png",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150206171146-03-kayla-muller-0206-large-169.jpg",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150206131757-01-yemen-unrest-0206-large-169.jpg",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150210173802-isis-manual-lead-pkg-02-10-large-169.jpg"]
-        }
-        popularStories: {
-            headline: {
-                link: ["http://www.cnn.com/videos/entertainment/2015/02/11/orig-jon-stewart-leaving-daily-show.comedy-central/video/playlists/most-popular-domestic/",
-                       "http://www.cnn.com/videos/world/2015/02/10/lead-bts-kayla-mueller-family-and-friends-press-conference.cnn/video/playlists/most-popular-domestic/",
-                       "http://www.cnn.com/videos/us/2015/02/10/newday-australia-car-chase.cnn/video/playlists/most-popular-domestic/",
-                       "http://www.cnn.com/videos/entertainment/2015/02/10/ac-dnt-kaye-nick-gordon-profile.cnn/video/playlists/most-popular-domestic/"],
-                description: ["See Stewart announce departure from 'The Daily Show'", 
-                              "Childhood friend: Kayla Mueller 'was joy'", 
-                              "High-speed car chase has dramatic ending",
-                              "Who is Nick Gordon?"]
-            }
-            image: ["http://i2.cdn.turner.com/cnnnext/dam/assets/150211004149-orig-jon-stewart-leaving-daily-show-00020622-medium-169.jpg", 
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150206133834-01-kayla-mueller-0206-medium-169.jpg",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150210072940-newday-australia-car-chase-medium-169.png",
-                    "http://i2.cdn.turner.com/cnnnext/dam/assets/150210210718-ac-dnt-kaye-nick-gordon-profile-00001513-medium-169.jpg"]
-        }
-        weatherInformation: {
-            location: "Ney York",
-            temperature: 32
-        }
-        
-    }
 });
