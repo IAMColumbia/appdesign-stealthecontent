efc8ed045b9a805e012fc5b0c28b822e0504709f
Matt Schick <matt@schickm.com>
2015-02-18 13:28:37 -0600

    fix syntax errors

--- client/stealthecontent.js
+++ client/stealthecontent.js
@@ -2,19 +2,19 @@ Template.main.helpers({
     data: {
      userInfo:{
 		 name: "Jake Mullen",
-		 username: "@jakeMullen1"",
+		 username: "@jakeMullen1",
 		 tweetCount: 1041,
 		 followingCount: 170,
 		 followerCount: 163,
 		 profliePicture: "https://pbs.twimg.com/profile_images/3644377202/e2d59daf9d8005564aad943cc0de8911_bigger.jpeg",
 		 backgroundPicture:"https://pbs.twimg.com/profile_images/3644377202/e2d59daf9d8005564aad943cc0de8911_bigger.jpeg",
-	 }
+	 },
 	 posts:[
 	 {
 		 type: "Text",
 		 name: "Wired",
 		 username: "@WIRED",
-		 postTime: "17m"
+		 postTime: "17m",
 		 postText: "Twitter is buying the talent agency, Niche, for an undisclosed sum http://wrd.cm/1AobEYX ",
 		 numRetweets: 8,
 		 numFavorites: 8,
@@ -40,7 +40,7 @@ Template.main.helpers({
 		 numRetweets: 120,
 		 numFavorites: 137
 	 }
-	]
+	],
 	 trends:{
 		 thingsTrending:["#WhatIsLoveIn4Words","House of Cards", "Valentines Day", "#ChapelHillShooting", "Jerry Tarkanian", "Willian", "#MUFC", "GBBOComicRelief", "Jackie Robinson West", "Jon Stewart"]
 		 
